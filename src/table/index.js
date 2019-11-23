b4274828e8ad65065b6f51b0880be6d0668d85af
Suryakant <kansu@amazon.com>
2019-11-23 19:45:56 +0530

    fetching data using useEffect

--- src/table/index.js
+++ src/table/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { connect } from 'react-redux';
 import TableHeader from './tableHeader';
 import TableBody from './tableBody';
 // import './table.css'
 
 const Table = (props) => {
+  let [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData(){
+      // fetch Data
+      let data = await fetch('https://jsonplaceholder.typicode.com/photos')
+                      .then(respone => respone.json());
+      console.log(data);
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div style={{'maxHeight': '400px', 'overflow': 'scroll', 'margin': '0 auto', 'fontSize': '10px', clear: 'both'}}>
       <table style={{width: '100%'}}>
-        <TableHeader />
-        <TableBody />
+        <TableHeader data={data} />
+        <TableBody data={data} />
       </table>
     </div>
   )
