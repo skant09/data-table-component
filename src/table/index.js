a41ae771ed0e5fb23f5e75064bb577b09a417f45
Suryakant <kansu@amazon.com>
2019-11-24 18:12:50 +0530

    creating checkbox component

--- src/table/index.js
+++ src/table/index.js
@@ -3,6 +3,20 @@ import { connect } from 'react-redux';
 import Table from './table';
 // import './table.css'
 
+const IndexComponent = props => {
+  const [hoverState, setHoverState] = useState();
+  const setCheckboxState = state => e => {
+    setHoverState(state);
+  }
+  if(hoverState){
+    return <input type="checkbox" value={props.id} checked onChange={console.log(props) || setCheckboxState(false)} />
+  }
+  return (
+    <span onClick={setCheckboxState(true)}>{props.albumId}</span>
+  )
+}
+
+
 const DataTable = (props) => {
   let [data, setData] = useState([]);
   useEffect(() => {
@@ -21,26 +35,31 @@ const DataTable = (props) => {
       columns={[{
         id: "albumId",
         label : "AlbumId",
-        element : data => <span>{data.albumId}</span>,
-        width: "100px"
+        element : IndexComponent,
+        width: "100px",
+        textAlign: 'right'
       }, {
         id: "id",
         label : "id",
         element : data => <span>{data.id}</span>,
-        width: "100px"
+        width: "100px",
+        textAlign: 'right'
       }, {
         id: "thumbnailUrl",
         label : "thumbnailUrl",
         element : data => <span>{data.thumbnailUrl}</span>,
-        width: "100px"
+        width: "100px",
+        textAlign: 'left'
       },{
         id: "title",
         label : "Title",
         element : data => <span>{data.title}</span>,
-        width: "100px"
+        width: "100px",
+        textAlign: 'left'
       }]}
     />
   )
 }
+
 
 export default DataTable;
