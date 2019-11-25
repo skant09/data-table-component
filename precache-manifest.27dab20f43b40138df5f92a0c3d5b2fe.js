403cf1f578be1e4f4849ac3b18e4f9ea28118c62
Suryakant <kansu@amazon.com>
2019-11-25 21:45:43 +0530

    Updates

---
+++ precache-manifest.27dab20f43b40138df5f92a0c3d5b2fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd84d258046f1f2d146643a1748e8af6",
+    "url": "/data-table-component/index.html"
+  },
+  {
+    "revision": "14620d17d9f0cf5dbc2c",
+    "url": "/data-table-component/static/css/main.bdd8903c.chunk.css"
+  },
+  {
+    "revision": "dfaeb633116dfdacfba5",
+    "url": "/data-table-component/static/js/2.3245c9c9.chunk.js"
+  },
+  {
+    "revision": "14620d17d9f0cf5dbc2c",
+    "url": "/data-table-component/static/js/main.9803776f.chunk.js"
+  },
+  {
+    "revision": "d2e285fc3f55e65318d4",
+    "url": "/data-table-component/static/js/runtime-main.5f18c751.js"
+  }
+]);
